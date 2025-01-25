c21e47ff5f123a8e0e2232211ef0987efb2428f1
SCFDMerch <tallan@live.com.au>
2025-01-25 17:10:10 +1100

    Nature pack

--- GGJ2025_Unity/Assets/Scripts/Building.cs
+++ GGJ2025_Unity/Assets/Scripts/Building.cs
@@ -9,4 +9,47 @@ public class Building : MonoBehaviour
     [SerializeField] private BuildingType _type;
     [SerializeField] private Location _location;
     [SerializeField] private string _name;
+
+    [SerializeField] private GameObject[] mansionOptions;
+    [SerializeField] private GameObject[] apartmentOptions;
+    [SerializeField] private GameObject[] houseOptions;
+
+    private void Start()
+    {
+        /*
+        // Determine which prefab options to use based on the building type
+        GameObject[] prefabOptions = null;
+
+        switch (_type)
+        {
+            case BuildingType.Mansion:
+                prefabOptions = mansionOptions;
+                break;
+            case BuildingType.Apartment:
+                prefabOptions = apartmentOptions;
+                break;
+            case BuildingType.House:
+                prefabOptions = houseOptions;
+                break;
+            default:
+                Debug.LogWarning("Unhandled building type: " + _type);
+                break;
+        }
+
+        // Spawn a prefab if options are available
+        if (prefabOptions != null && prefabOptions.Length > 0)
+        {
+            int randomIndex = Random.Range(0, prefabOptions.Length);
+            GameObject selectedPrefab = prefabOptions[randomIndex];
+
+            // Instantiate the selected prefab and set it as a child
+            GameObject instance = Instantiate(selectedPrefab, transform.position, Quaternion.identity);
+            instance.transform.SetParent(transform);
+        }
+        else
+        {
+            Debug.LogWarning($"No prefab options set for the building type {_type}!");
+        }
+        */
+    }
 }
